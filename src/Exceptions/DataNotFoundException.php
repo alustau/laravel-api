865bb8214e619c3a3177b2293f03e265ec56e6b6
alustau <denisalustau@gmail.com>
2016-08-24 12:47:16 -0300

    Adding exception

---
+++ src/Exceptions/DataNotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alustau\API\Exception;
+
+use Exception;
+
+/**
+ * Description of DataNotFoundException
+ *
+ * @author alustau
+ */
+class DataNotFoundException extends Exception
+{
+     /**
+     * Create a new exception instance.
+     *
+     * @param  string $message
+     * @param  \Illuminate\Http\Response  $response
+     * @return void
+     */
+    public function __construct($message, $response = null)
+    {
+        parent::__construct('The results of given data was not found', 404);
+    }
+}
