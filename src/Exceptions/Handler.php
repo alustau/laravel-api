b64174ed21a3de416f0572fd0c3ddd7f25f3cb14
alustau <denisalustau@gmail.com>
2016-08-24 12:55:04 -0300

    Adding json return

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -45,6 +45,26 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        $response = [
+            'message' => (string) $e->getMessage(),
+            'status' => 400
+        ];
+        
+        if ($e instanceof HttpException) {
+            $response['message'] = Response::$statusTexts[$e->getStatusCode()];
+            $response['status'] = $e->getStatusCode();
+        } else if ($e instanceof ModelNotFoundException) {
+            $response['message'] = Response::$statusTexts[Response::HTTP_NOT_FOUND];
+            $response['status'] = Response::HTTP_NOT_FOUND;
+        } 
+
+//        if ($this->isDebugMode()) {
+//            $response['debug'] = [
+//                'exception' => get_class($e),
+//                'trace' => $e->getTrace()
+//            ];
+//        }
+
+        return response()->json(['error' => $response], $response['status']);
     }
 }
