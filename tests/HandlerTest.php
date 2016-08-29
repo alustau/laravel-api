4c670b5939eab34dd100bcd0e5c5164002f537fa
alustau <denisalustau@gmail.com>
2016-08-29 16:09:59 -0300

    Changing HandlerTest

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -4,13 +4,21 @@ use PHPUnit\Framework\TestCase;
 use Alustau\API\Exceptions\Handler;
 use Alustau\API\Exceptions\DataNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class HandlerTest extends TestCase
 {
     public function testRenderJson()
     {
-        $this->assertJson(
-            (new Handler)->render(new Request, new DataNotFoundException)
-        );
+        $response = (new Handler)->render(new Request, new DataNotFoundException);
+        
+        $this->assertJson($response->getContent());
+    }
+    
+    public function testInstanceOfHandler()
+    {
+        $response = (new Handler)->render(new Request, new DataNotFoundException);
+        
+        $this->assertInstanceOf(JsonResponse::class, $response);
     }
 }
